c3f86d35643818bcafa2776ebd1ad3ec1afe0cfd
Rodrigo Junior <rodrigojunior0323@gmail.com>
2024-06-14 17:25:36 -0300

    Finalização do app.

--- app/src/main/java/com/example/agendamedicaon/model/solicitarAgendamento.kt
+++ app/src/main/java/com/example/agendamedicaon/model/solicitarAgendamento.kt
@@ -3,5 +3,6 @@ package com.example.agendamedicaon.model
 data class AgendamentoRequest(
     val medicoId: Int,
     val horarioId: Int,
-    val pacienteId: Int
+    val pacienteId: Int,
+    val especialidadeNome: String
 )
--- app/src/main/java/com/example/agendamedicaon/view/HomeActivity.kt
+++ app/src/main/java/com/example/agendamedicaon/view/HomeActivity.kt
@@ -5,17 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.agendamedicaon.R
-import com.example.agendamedicaon.model.AgendamentoRequest
-import com.example.agendamedicaon.model.Especialidade
-import com.example.agendamedicaon.model.Medico
-import com.example.agendamedicaon.model.Horario
-import com.example.agendamedicaon.model.Paciente
+import com.example.agendamedicaon.model.*
 import com.example.agendamedicaon.repository.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,8 +22,10 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var spinnerMedicos: Spinner
     private lateinit var spinnerHorarios: Spinner
     private lateinit var buttonSolicitarAgendamento: Button
+    private lateinit var buttonVerConsultas: Button
     private lateinit var selectedMedico: Medico
     private lateinit var selectedHorario: Horario
+    private lateinit var selectedEspecialidade: Especialidade
     private var paciente: Paciente? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +38,7 @@ class HomeActivity : AppCompatActivity() {
         spinnerMedicos = findViewById(R.id.spinnerMedicos)
         spinnerHorarios = findViewById(R.id.spinnerHorarios)
         buttonSolicitarAgendamento = findViewById(R.id.buttonSolicitarAgendamento)
+        buttonVerConsultas = findViewById(R.id.buttonVerConsultas)
 
         loadEspecialidades()
 
@@ -48,9 +46,9 @@ class HomeActivity : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val especialidade = parent?.getItemAtPosition(position) as Especialidade
-                Log.d("Especialidade", especialidade.id.toString())
-                loadMedicos(especialidade.id)
+                selectedEspecialidade = parent?.getItemAtPosition(position) as Especialidade
+                Log.d("Especialidade", selectedEspecialidade.nome)
+                loadMedicos(selectedEspecialidade.id)
             }
         }
 
@@ -72,10 +70,15 @@ class HomeActivity : AppCompatActivity() {
         }
 
         buttonSolicitarAgendamento.setOnClickListener {
-            solicitarAgendamento(selectedMedico.id, selectedHorario.id)
+            solicitarAgendamento(selectedMedico.id, selectedHorario.id, selectedEspecialidade.nome)
+        }
+
+        buttonVerConsultas.setOnClickListener {
+            val intent = Intent(this@HomeActivity, ConsultasAgendadasActivity::class.java)
+            intent.putExtra("idPaciente", paciente?.id)
+            startActivity(intent)
         }
     }
-
 
     private fun loadEspecialidades() {
         val call = RetrofitClient.apiService.getEspecialidades()
@@ -84,6 +87,7 @@ class HomeActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<Especialidade>>, response: Response<List<Especialidade>>) {
                 if (response.isSuccessful) {
                     val especialidades = response.body()
+                    Log.d("EspecialidadesResponse", "Recebido: $especialidades")
                     if (!especialidades.isNullOrEmpty()) {
                         val adapter = EspecialidadeAdapter(this@HomeActivity, R.layout.spinner_item, especialidades)
                         spinnerEspecialidades.adapter = adapter
@@ -111,6 +115,7 @@ class HomeActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<Medico>>, response: Response<List<Medico>>) {
                 if (response.isSuccessful) {
                     val medicos = response.body()
+                    Log.d("MedicosResponse", "Recebido: $medicos")
                     if (!medicos.isNullOrEmpty()) {
                         val adapter = MedicoAdapter(this@HomeActivity, R.layout.spinner_medico_item, medicos)
                         spinnerMedicos.adapter = adapter
@@ -139,6 +144,7 @@ class HomeActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<Horario>>, response: Response<List<Horario>>) {
                 if (response.isSuccessful) {
                     val horarios = response.body()
+                    Log.d("HorariosResponse", "Recebido: $horarios")
                     if (!horarios.isNullOrEmpty()) {
                         val adapter = HorarioAdapter(this@HomeActivity, R.layout.spinner_horario_item, horarios)
                         spinnerHorarios.adapter = adapter
@@ -161,15 +167,17 @@ class HomeActivity : AppCompatActivity() {
         })
     }
 
-
-    private fun solicitarAgendamento(medicoId: Int, horarioId: Int) {
+    private fun solicitarAgendamento(medicoId: Int, horarioId: Int, especialidadeNome: String) {
         val paciente = intent.getParcelableExtra<Paciente>("paciente")
         if (paciente == null) {
             Toast.makeText(this, "Erro: Dados do paciente não encontrados.", Toast.LENGTH_SHORT).show()
             return
         }
 
-        val request = AgendamentoRequest(medicoId, horarioId, paciente.id)
+        val request = AgendamentoRequest(medicoId, horarioId, paciente.id, especialidadeNome)
+
+        // Adicionando log para verificar o que está sendo enviado para o backend
+        Log.d("AgendamentoRequest", "MedicoId: $medicoId, HorarioId: $horarioId, PacienteId: ${paciente.id}, EspecialidadeNome: $especialidadeNome")
 
         val call = RetrofitClient.apiService.solicitarAgendamento(request)
 
